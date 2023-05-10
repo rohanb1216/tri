261f312180694175ee4e297453ce25be9bf0d350
Rohan <rohan.biju@gmail.com>
2023-05-10 21:34:52 +0530

    tri

---
+++ cmd/list.go
@@ -0,0 +1,63 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"text/tabwriter"
+
+	"github.com/rohanb1216/tri/todo"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// listCmd represents the list command
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "list todos",
+	Long:  `list todooooos`,
+	Run:   listRun,
+}
+
+var (
+	doneOpt bool
+	allOpt  bool
+)
+
+func listRun(cmd *cobra.Command, args []string) {
+	items, err := todo.ReadItems(viper.GetString("datafile"))
+	if err != nil {
+		log.Printf("%v", err)
+	}
+
+	sort.Sort(todo.ByPri(items))
+
+	w := tabwriter.NewWriter(os.Stdout, 3, 0, 1, ' ', 0)
+	for _, i := range items {
+		if allOpt || i.Done == doneOpt {
+			fmt.Fprintln(w, i.Label()+"\t"+i.PrettyDone()+"\t"+i.PrettyP()+"\t"+i.Text+"\t")
+		}
+	}
+	w.Flush()
+}
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// listCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	listCmd.Flags().BoolVar(&doneOpt, "done", false, "Show 'done' ToDos")
+	listCmd.Flags().BoolVar(&allOpt, "all", false, "Show all ToDos")
+
+}
--- cmd/root.go
+++ cmd/root.go
@@ -4,10 +4,17 @@ Copyright © 2023 rohan
 package cmd
 
 import (
+	"fmt"
+	"log"
 	"os"
 
+	"github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
+
+var datafile string
+var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -32,10 +39,29 @@ func init() {
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
+	cobra.OnInitialize(initConfig)
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.src.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.tri.yaml)")
+
+	home, err := homedir.Dir()
+
+	if err != nil {
+		log.Println("Unable to find home directory. Please set data file using --datafile.")
+	}
+
+	rootCmd.PersistentFlags().StringVar(&datafile, "datafile", home+string(os.PathSeparator)+".tridos.json", "data file to store todos")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func initConfig() {
+	viper.SetConfigName(".tri")
+	viper.AddConfigPath("$HOME")
+	viper.AutomaticEnv()
+	viper.SetEnvPrefix("tri")
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file: ", viper.ConfigFileUsed())
+	}
 }
